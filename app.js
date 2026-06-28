3132603e17048c40c1a2a6870022a54dce414acd
MathewAyala <mathewayala05@gmail.com>
2026-06-27 22:12:57 -0400

    Part 2: complete

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let careNotes = [
+  { id: 1, plantId: 1, note: "Needs water every 2 weeks." },
+  { id: 2, plantId: 1, note: "Tolerates low light well." },
+  { id: 3, plantId: 3, note: "Loves humidity." },
+];
+
+let nextNoteId = 4;
+//--------------------------------------------------------------------------------------
 let plants = [
   { id: 1, name: "Snake Plant", type: "Succulent", sunlight: "Low", watered: true },
   { id: 2, name: "Pothos", type: "Vine", sunlight: "Medium", watered: false },
@@ -12,53 +20,77 @@ const app = express();
 const PORT = 8080;
 app.use(express.json());
 
-
-// app.get('/api/plants', (request, response) =>{
-//     response.json(plants)
+//this is to retireve all plants
+// app.get('/api/plants', (req, res) =>{
+//     res.json(plants)
 // } )
-
+//---------------------------------------------------------------------------
+//req.query
 //the query values are set to lowercase
-app.get('/api/plants', (request, response) =>{
-    const {type,sunlight} = request.query;
-    const plantType = plants.filter(plant => plant.type.toLowerCase() === type)
+//still need to find a way to accept one, the other, and both. Im thinking maybe middleware
+app.get('/api/plants', (req, res) =>{
+    const {type,sunlight} = req.query;
+    const plantType = plants.filter(target => target.type.toLowerCase() === type)
     const amountOfSunlight = plants.filter(amount => amount.sunlight.toLowerCase() === sunlight)
     
-    response.json(plantType || amountOfSunlight)
+    res.json(plantType || amountOfSunlight)
+} )
+app.get('/api/notes', (req, res) => {
+    res.json(careNotes);
+} )
+app.get('/api/plants/:plantId/notes', (req, res) =>{
+    const plantId = Number(req.params.plantId); //REMEMBER THIS!!! plantId from req.params is based off the URL not the property 
+    const plantNote = careNotes.filter((target) => target.plantId === plantId)    
+    res.json(plantNote)
 } )
 
-app.get('/api/plants/:id', async (request, response) =>{
-    const plantId = Number(request.params.id);
+app.get('/api/plants/:id', async (req, res) =>{
+    const plantId = Number(req.params.id);
     const plant = plants.find((target) => target.id === plantId)
     await  new Promise ((resolve) => setTimeout(resolve,500)) 
     plant
-    ? response.json(plant) 
-    : response.status(404).send("Plant not found")
+    ? res.json(plant) 
+    : res.status(404).send("Plant not found")
 } )
 
-app.post('/api/plants', (request, response) =>{
-    const {name, type, sunlight} = request.body 
+app.post('/api/plants', (req, res) =>{
+    const {name, type, sunlight} = req.body 
     const newPlant = {id: nextId, name, type, sunlight, watered: false }
     nextId++;
     plants.push(newPlant);
-    response.status(201).json(newPlant);
+    res.status(201).json(newPlant);
 } )
 
-app.patch('/api/plants/:id', (request, response) =>{
-    const plantId = Number(request.params.id)
+app.post('/api/plants/:plantId/notes', (req, res) =>{
+    const plantId = Number(req.params.plantId);
+    const {note} = req.body;
+    const newNote = {id: nextNoteId, plantId: plantId, note};
+    nextNoteId++;
+    careNotes.push(newNote);
+    res.status(201).json(newNote);
+} )
+
+app.patch('/api/plants/:id', (req, res) =>{
+    const plantId = Number(req.params.id)
     const plantToUpdate = plants.find((target) => target.id === plantId)
     plantToUpdate 
-    ? Object.assign(plantToUpdate, request.body) 
-    && response.status(201).json(plantToUpdate) 
-    : response.status(404).send('Plant not found')
+    ? Object.assign(plantToUpdate, req.body) 
+    && res.status(201).json(plantToUpdate) 
+    : res.status(404).send('Plant not found')
     
 } )
 
-app.delete('/api/plants/:id', (request, response) =>{
-    const plantId = Number(request.params.id);
+app.delete('/api/plants/:id', (req, res) =>{
+    const plantId = Number(req.params.id);
     plants = plants.filter((target) => { return target.id !== plantId});
     console.log(`Plant ${plantId} deleted`)
-    response.status(204).send('Plant deleted');
+    res.status(204).send();
 
 } )
 
+app.delete('/api/notes/:id', (req, res) =>{
+    const noteId = Number(req.params.id);
+    careNotes = careNotes.filter(note => note.id !== noteId)
+    res.status(204).send();
+} )
 app.listen(PORT, () => console.log('Server Running on port 8080') )
